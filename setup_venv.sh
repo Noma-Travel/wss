2e0c0047c339473662653a15085124319fdaff89
Lucas Toscano <92105205+LucasToscanou@users.noreply.github.com>
2026-04-24 08:24:16 -0300

    Update setup_venv.sh

--- setup_venv.sh
+++ setup_venv.sh
@@ -17,7 +17,6 @@ source wss-venv/bin/activate
 echo "Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
-pip install --upgrade pip
 
 echo "Setup complete! To activate the virtual environment, run:"
 echo "  source wss-venv/bin/activate"
